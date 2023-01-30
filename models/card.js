c9953d1928971538cda985b84d41c5953101696c
Kenneth Holt <kingholt@gmail.com>
2023-01-30 13:01:15 -0800

    added files, folders, server is up, working on api

--- models/card.js
+++ models/card.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const cardSchema = new Schema({
+  name: { type: String, required: true },
+  uri: String,
+});
+
+const CardModel = mongoose.model('card', cardSchema);
+
+module.exports = CardModel;
+
